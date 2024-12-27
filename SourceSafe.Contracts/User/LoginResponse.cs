2be3d7b48c59d3b395c8118f55f8b7dcb7444e6a
RamiH20 <120262594+RamiH20@users.noreply.github.com>
2024-12-27 19:59:25 +0300

    refresh token

--- SourceSafe.Contracts/User/LoginResponse.cs
+++ SourceSafe.Contracts/User/LoginResponse.cs
@@ -1,5 +1,4 @@
 ﻿using System.Net;
-using System.Text.Json.Serialization;
 
 namespace SourceSafe.Contracts.User;
 
@@ -7,7 +6,6 @@ public class LoginResponse
 {
     public HttpStatusCode Status { get; set; }
     public string Token { get; set; } = null!;
-    [JsonIgnore]
     public string RefreshToken { get; set; } = null!;
     public DateTime RefreshTokenExpiration { get; set; }
 }
