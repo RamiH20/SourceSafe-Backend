b054028b5ca11a7d91d666354d056d1556fbb958
RamiH20 <120262594+RamiH20@users.noreply.github.com>
2025-01-22 01:58:26 +0300

    reports

--- SourceSafe.Application/Common/DTOs/UserReportDTO.cs
+++ SourceSafe.Application/Common/DTOs/UserReportDTO.cs
@@ -2,6 +2,6 @@
 
 public class UserReportDTO
 {
-    public int UserId { get; set; }
+    public string UserName { get; set; } = null!;
     public List<FilesUserReportDTO> Files { get; set; } = [];
 }
